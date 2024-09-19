0c93a2b771dbadbd422eb5366b5a8830ee923058
Rehan Mondal <79845546+rehan13@users.noreply.github.com>
2024-09-19 11:56:30 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import project1 from "../assets/projects/project-1.png";
-import project2 from "../assets/projects/project-2.png";
+import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 import project5 from "../assets/projects/project-1.jpg";
